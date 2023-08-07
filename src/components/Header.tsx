68933ee1061e2119d47c633791ccc7f8606fea3c
allyleandre <tuyambazeleandre@gmail.com>
2023-08-07 15:15:33 +0200

    ch: add react-router-dom

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className=' bg-[#37475A] w-full justify-start py-4 hidden sm:flex '>
+      <div className='ml-24 text-white'>Elevate Your Feedback Experience</div>
+    </div>
+  );
+};
+
+export default Header;
